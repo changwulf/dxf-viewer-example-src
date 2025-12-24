5fef9f21839f5a7c81b352b2361f046d7cd541f1
changwulf <changwulf@gmail.com>
2025-12-24 10:02:17 +0800

    Updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,11 +6,23 @@ module.exports = {
     transpileDependencies: [
         /[\\\/]node_modules[\\\/]dxf-viewer[\\\/]/
     ],
+    devServer: {
+        port: 9001,
+        host: '0.0.0.0'
+    },
     configureWebpack: {
         plugins: [
             new DefinePlugin({
                 "DXF_VIEWER_VERSION": JSON.stringify(dxfViewerPackageJson.version)
             })
-        ]
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.worker\.js$/,
+                    use: { loader: 'worker-loader' }
+                }
+            ]
+        }
     }
 }
